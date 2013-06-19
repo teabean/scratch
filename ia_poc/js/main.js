dcc813dd93431d8efb826e4c0052579e1844e8ff
Tim Seed <timseed@gmail.com>
2013-06-19 15:02:15 +0200

    Use json data
    Also, get rid of html demo, it's not any faster.

--- ia_poc/js/main.js
+++ ia_poc/js/main.js
@@ -1,6 +1,11 @@
 define(["jquery", "mustache", "text!../templates/box_template.html"], function($, mustache, template) {
     var content = $("#content");
     
+    $(window).scroll(function(eventObject) {
+        // do stuff! You can find out about how far the window has scrolled via the eventObject.
+/*         console.log(eventObject); */
+    });
+    
     var running_mustache = false;
     var running_html = false;
     
@@ -22,7 +27,6 @@ define(["jquery", "mustache", "text!../templates/box_template.html"], function($
                 if (running_mustache) {
                     mustache_add_box();
                     setTimeout(arguments.callee, Math.random() * 1500);
-                    
                 } 
                 
                 if (i > 500) {
@@ -35,61 +39,103 @@ define(["jquery", "mustache", "text!../templates/box_template.html"], function($
     }); 
     
     var mustache_add_box = function() {
-        var gradient = gradient_array[Math.floor(Math.random() * gradient_array.length)];
-        var amount = Math.floor(Math.random() * 100);
-        var bet_type = bet_type_array[Math.floor(Math.random() * bet_type_array.length)];
-        var bet_description = bet_descriptions_array[Math.floor(Math.random() * bet_descriptions_array.length)];
-        
-        var result = mustache.render(template, { 
-            "background_gradient" : gradient,
-            "left_content" : "£" + amount + ".00", 
-            "left_sub_label" : bet_type, 
-            "account_description" : "Tim Seed", 
-            "account_username" : "seedt",
-            "account_number" : "7428910",
-            "description" : "GBP "  + amount + ".00 " + bet_type + bet_description,
-            "right_footer_left" : "Potential Win £" + (amount * 3) + ".00",
-            "right_footer_middle" : "Total Stake £" + amount + ".00",
-            "right_footer_right" : "45s ago"
-        });
-        
-        content.prepend(result);
-    };
-    
-    $("#html_populate").click(function() {
-        var i = 0;
-        
-        if (running_html) {
-            running_html = false;
-            $("#html_populate").text("Start HTML Populate");
-            i = 0;
-        } else {
-            content.empty();
-            running_html = true;
-            runnning_mustache = false;
-            $("#html_populate").text("Stop HTML Populate");
+        $.getJSON("json/test.js", function(data) {
+            var test_data = data.mydata[Math.floor(Math.random() * data.mydata.length)];
+            console.log(test_data);
             
-            setTimeout(function() {
-                i++;
-                if (running_html) {
-                    html_add_box();
-                    setTimeout(arguments.callee, Math.random() * 1500);
+            var type = test_data.type;
+            var gradient = null;
+            switch (type) {
+                case "bet":
+                    gradient = "bggold";
+                    break;
+                case "new_account":
+                    gradient = "bggreen";
+                    break;
+                case "failed_bet":
+                    gradient = "bgred";
+                    break;
+                case "payrec" :
+                    gradient = "bgbronze";
+                    break;
+            }
+/*             gradient_array[Math.floor(Math.random() * gradient_array.length)]; */
+            var amount = Math.floor(Math.random() * 100);
+            
+            var bet_create_date = new Date();
+            var time = new Date();
+            
+            var result = mustache.render(template, { 
+                "background_gradient" : gradient,
+                "left_content" : "£" + amount + ".00", 
+                "left_sub_label" : type === "bet" ? test_data.bet_type : type, 
+                "account_description" : test_data.account_description, 
+                "account_username" : test_data.username,
+                "account_number" : test_data.account_number,
+                "description" : test_data.description,
+                "right_footer_left" : "Potential Win £" + (amount * 3) + ".00",
+                "right_footer_middle" : "Total Stake £" + amount + ".00",
+                "right_footer_right" : time          
+            });
+            
+            content.prepend(result);
+            
+            // This kills it.
+    /*         animate($("#content .box").first(), "animated bounceInLeft"); */
+            
+            var justAdded = $("#content .box").first();
+            var timeSection = justAdded.find(".right_footer_right");
+/*             justAdded.attr("id", time.getTime()); */
+            
+            // We can add the json data to the added item for future reference
+            justAdded.data("data", test_data);
+            
+            window.setTimeout(function() {
+                var date = new Date();
+                var seconds = Math.round((date.getTime() - bet_create_date.getTime()) / 1000);
+                var minutes = 59;
+                
+                var prettyDate = seconds + "s ago";
+                
+                // We can play with this timeout to make things easier on the cpu
+                var newTimeout = 15000;
+                
+                if (seconds > 59) {
+                  minutes = Math.floor(seconds / 60);
+                  prettyDate = minutes + "m ago";
+                  newTimeout = 20000;
+                }  
+                
+                if (minutes > 59 && seconds > 59) {
+                  var hours = Math.floor(minutes / 60);
+                  prettyDate = hours + "h ago";
+                  newTimeout = 60000;
                 }
-            }, 500);
-        }
-    });
+          
+                timeSection.html("About " + prettyDate);
+                setTimeout(arguments.callee, newTimeout);
+            }, 0);
+            
+            justAdded.click(function() {
+                console.log(justAdded.data("data").username); 
+            });
+
+        });
     
-    var html_add_box = function() {
-        content.prepend('<section class="box"><section class="left bglightblue2"><section class="left_content">&pound;5.00</section><section class="left_label">Single</section></section><section class="right"><section class="account_details"><section class="account_description">Timothy Seed</section><section class="account_username">seedt</section><section class="account_number">7428910</section></section><section class="description">GBP 5.00 Single: ARSENAL @ 2.00 [Win/Draw/Win - 90 Mins] - Arsenal vs Wigan Athletic Premier League</section><section class="right_footer"><section class="right_footer_left">Potential Win &pound;10.00</section><section class="right_footer_middle">Total Stake &pound;5.00</section><section class="right_footer_right">9m ago</section></section></section></section>');
     };
     
-    var gradient_array = ["bglightblue", "bglightblue2", "bggreen", "bgred", "bggold", "bgpink", "bgbronze", "bgorangey"];
-    var bet_type_array = ["Single", "Double", "Treble", "4-Fold", "Yankee", "10-Fold", "Patent", "System", "Scooby-Doo", "5-Fold", "6-Fold"];
-    var bet_descriptions_array = [
-        ": DRAW @ 3.00 [Win/Draw/Win - 90 Mins] - Everton vs Chelsea Premier League; LIVERPOOL @ 4.00 [Win/Draw/Win - 90 Mins] - Liverpool vs Arsenal Premier League",
-        ": WIGAN ATHLETIC @ 4.00 [Win/Draw/Win - 90 Mins] - Arsenal vs Wigan Athletic Premier League",
-        ": MANCHESTER UNITED @ 2.00 [Win/Draw/Win - 90 Mins] - Manchester United vs Sunderland Premier League",
-        ": ARSENAL @ 2.00 [Win/Draw/Win - 90 Mins] - Arsenal vs Wigan Athletic Premier League; WIGAN ATHLETIC @ 4.00 [Win/Draw/Win - 90 Mins] - Arsenal vs Wigan Athletic Premier League; MANCHESTER UNITED @ 2.00 [Win/Draw/Win - 90 Mins] - Manchester United vs Sunderland Premier League"
-    ];
+    // Perform the given css animation on the given element. Once complete we remove the css animation so the element can be animated again later if we need.
+    function animate(element, animation) {
+        element.addClass(animation);
+        
+        // Can't call clearAnimation here as browser caches styling changes until the js has finished executing and then performs them in one go. 
+        // So we end up seeing no animation. Instead we can check once the animation has finished and then remove the animation classes.
+        element.on("webkitAnimationEnd", function() {
+            // Note this is only for webkit (Not that we are worried about anything but webkit, other browsers use different names e.g. oanimationend for Opera)
+            clearAnimation(element, animation);
+        });
+    }
+    
+    var gradient_array = ["bglightblue", "bglightblue2", "bggold", "bgpink", "bgbronze", "bgorangey"];
 });
 
---
+++ ia_poc/json/test.js
@@ -0,0 +1,69 @@
+{
+    "mydata" : [
+        {
+            "type" : "bet",
+            "id" : "1234",
+            "username" : "bondj",
+            "account_description" : "James Bond",
+            "account_number" : "85204823",
+            "description" : "GBP 5.00 Single DRAW @ 3.00 [Win/Draw/Win - 90 Mins] - Everton vs Chelsea Premier League; LIVERPOOL @ 4.00 [Win/Draw/Win - 90 Mins] - Liverpool vs Arsenal Premier League",
+            "amount" : "5.00",
+            "bet_type" : "Single"
+        }, 
+        {
+            "type" : "bet",
+            "id" : "1235",
+            "username" : "seedt",
+            "account_description" : "Tim Seed",
+            "account_number" : "91849204",
+            "description" : "GBP 25.00 Single MANCHESTER UNITED @ 2.00 [Win/Draw/Win - 90 Mins] - Manchester United vs Sunderland Premier League",
+            "amount" : "25.00",
+            "bet_type" : "Single"
+        },
+        {
+            "type" : "bet",
+            "id" : "1236",
+            "username" : "becks",
+            "account_description" : "Daivd Beckham",
+            "account_number" : "34922355",
+            "description" : "GBP 100.00 Single WIGAN ATHLETIC @ 4.00 [Win/Draw/Win - 90 Mins] - Arsenal vs Wigan Athletic Premier League",
+            "amount" : "100.00",
+            "bet_type" : "Single"
+        },
+        {
+            "type" : "bet",
+            "id" : "1237",
+            "username" : "woodst",
+            "account_description" : "Tiger Woods",
+            "account_number" : "111243252",
+            "description" : "GBP 12.00 Treble ARSENAL @ 2.00 [Win/Draw/Win - 90 Mins] - Arsenal vs Wigan Athletic Premier League; WIGAN ATHLETIC @ 4.00 [Win/Draw/Win - 90 Mins] - Arsenal vs Wigan Athletic Premier League; MANCHESTER UNITED @ 2.00 [Win/Draw/Win - 90 Mins] - Manchester United vs Sunderland Premier League",
+            "amount" : "12.00",
+            "bet_type" : "Treble"
+        },
+        {
+            "type" : "new_account",
+            "id" : "5678",
+            "username" : "sugara",
+            "account_description" : "Alan Sugar",
+            "account_number" : "12514532",
+            "description" : " max_powerz@gmail.com<br>Referred by Joe Johnson"
+        }, 
+        {
+            "type" : "payrec",
+            "id" : "98765",
+            "username" : "jordanm",
+            "account_description" : "Michael Jordan",
+            "account_number" : "23232323",
+            "description" : "PAYMENT/RECEIPT, Bank Deposit: Customer requested £1000 to be paid to Bank Account",
+            "amount" : "1000.00"
+        }, 
+        {
+            "type" : "failed_bet",
+            "id" : "1111",
+            "username" : "schumacerm",
+            "account_description" : "Michael Schumacer",
+            "account_number" : "4987432",
+            "description" : "[RES-00048]Betting on event Everton vs Chelsea is currently suspended. GBP 50.00 Single: EVERTON @ 4.00 [Win/Draw/Win - 90 Mins] - Everton vs Chelsea Premier League"
+        }
+    ]    
+}
