ef811b6064ef44d4558f57648d1b735856156356
Tim Seed <timseed@gmail.com>
2013-07-11 11:34:02 +0200

    Lots of updates to ia_poc

--- ia_poc/js/main.js
+++ ia_poc/js/main.js
@@ -4,30 +4,150 @@ define(["jquery", "mustache", "text!../templates/box_template.html", "abpify"], 
     
     var content = $("#content");
     var hideBets = false;
-    var running_mustache = false;
-    var fromKey = -1;
+    var betsFromKey = -1;
+    var accountsFromKey = -1;
+    var failedBetsFromKey = -1;
+    var payrecsFromKey = -1;
+    var betSlipsFromKey = -1;
+    
+    var isAutoFetching = false;
+    var autoFetchMillis = 5000;
+    var fetchInterval = null;
+    
+    $("#auto_fetch").click(function() {
+        if (isAutoFetching) {
+            isAutoFetching = false;
+            $("#auto_fetch").text("Start Auto Fetch");
+            window.clearInterval(fetchInterval);
+        } else {
+            isAutoFetching = true;
+            $("#auto_fetch").text("Stop Auto Fetch");
+            
+            fetchInterval = window.setInterval(function () {
+                ABPIFY.ajax("GET", "/rest/instantaction?fromKey=" + -1 + "&type=ALL&betsFromKey=" + betsFromKey + "&accountsFromKey=" + accountsFromKey + "&failedBetsFromKey=" + failedBetsFromKey + "&transactionsFromKey=" + payrecsFromKey + "&betSlipsFromKey=" + betSlipsFromKey, null, function(data) {
+    			if (data) {
+                    betsFromKey = data.lastBetsKey;
+                    accountsFromKey = data.lastAccountsKey;
+                    failedBetsFromKey = data.lastFailedBetsKey;
+                    payrecsFromKey = data.lastTransactionsKey;
+                    betSlipsFromKey = data.lastBetSlipsKey;
+                    
+                    for (var i = 0; i < data.betList.length; i++) {
+                        add_box(data.betList[i], "bet");
+                    }
+                    
+                    for (var i = 0; i < data.accountList.length; i++) {
+                        add_box(data.accountList[i], "new_account");
+                    }
+                    
+                    for (var i = 0; i < data.failedBetsList.length; i++) {
+                        add_box(data.failedBetsList[i], "failed_bet");
+                    }
+    			    
+                    for (var i = 0; i < data.transactionList.length; i++) {
+                        add_box(data.transactionList[i], "payrec");
+                    }
+    			}
+    			})
+            }, autoFetchMillis);
+        }
+    });
+    
+    $("#fetch_all").click(function() {
+        ABPIFY.ajax("GET", "/rest/instantaction?fromKey=" + -1 + "&type=ALL&betsFromKey=" + betsFromKey + "&accountsFromKey=" + accountsFromKey + "&failedBetsFromKey=" + failedBetsFromKey + "&transactionsFromKey=" + payrecsFromKey + "&betSlipsFromKey=" + betSlipsFromKey, null, function(data) {
+			if (data) {
+			    console.log(data);
+                betsFromKey = data.lastBetsKey;
+                accountsFromKey = data.lastAccountsKey;
+                failedBetsFromKey = data.lastFailedBetsKey;
+                payrecsFromKey = data.lastTransactionsKey;
+                betSlipsFromKey = data.lastBetSlipsKey;
+                
+                for (var i = 0; i < data.betList.length; i++) {
+                    add_box(data.betList[i], "bet");
+                }
+                
+                for (var i = 0; i < data.accountList.length; i++) {
+                    add_box(data.accountList[i], "new_account");
+                }
+                
+                for (var i = 0; i < data.failedBetsList.length; i++) {
+                    add_box(data.failedBetsList[i], "failed_bet");
+                }
+			    
+                for (var i = 0; i < data.transactionList.length; i++) {
+                    add_box(data.transactionList[i], "payrec");
+                }
+			}
+		});
+    });
     
     $("#fetch_bets").click(function() {
-		ABPIFY.ajax("GET", "/rest/instantaction?fromKey=" + fromKey + "&type=BETS", null, function(data) {
+		ABPIFY.ajax("GET", "/rest/instantaction?fromKey=" + betsFromKey + "&type=BETS", null, function(data) {
 			if (data) {
-			    fromKey = data.lastKey;
+			    betsFromKey = data.lastBetsKey;
 			    for (var i = 0; i < data.betList.length; i++) {
     			    console.log(data.betList[i]);
-    			    add_box(data.betList[i]);
+    			    add_box(data.betList[i], "bet");
 			    }
 			}
 		});
 	});
 	
-	var add_box = function(data) {
-        var type = "bet";
+	$("#fetch_accounts").click(function() {
+		ABPIFY.ajax("GET", "/rest/instantaction?fromKey=" + accountsFromKey + "&type=ACCOUNTS", null, function(data) {
+			if (data) {
+			    accountsFromKey = data.lastAccountsKey;
+			    for (var i = 0; i < data.accountList.length; i++) {
+    			    console.log(data.accountList[i]);
+    			    add_box(data.accountList[i], "new_account");
+			    }
+			}
+		});
+	});
+	
+	$("#fetch_failed_bets").click(function() {
+		ABPIFY.ajax("GET", "/rest/instantaction?fromKey=" + failedBetsFromKey + "&type=FAILED_BETS", null, function(data) {
+			if (data) {
+			    failedBetsFromKey = data.lastFailedBetsKey;
+			    for (var i = 0; i < data.failedBetsList.length; i++) {
+    			    console.log(data.failedBetsList[i]);
+    			    add_box(data.failedBetsList[i], "failed_bet");
+			    }
+			}
+		});
+	});
+	
+	$("#fetch_payrecs").click(function() {
+		ABPIFY.ajax("GET", "/rest/instantaction?fromKey=" + payrecsFromKey + "&type=TRANSACTIONS", null, function(data) {
+			if (data) {
+			    payrecsFromKey = data.lastTransactionsKey;
+			    for (var i = 0; i < data.transactionList.length; i++) {
+    			    console.log(data.transactionList[i]);
+    			    add_box(data.transactionList[i], "payrec");
+			    }
+			}
+		});
+	});
+	
+	var add_box = function(data, type) {
+    	// Store the type for later use
         data.type = type;
-        var amount = data.unitStake;
+        
 
-        // Get the colour for the left hand box
+        // Get the colour for the left hand box and the label descriptions
         var gradient = null;
+        
+        // Data differs based on type
+        var amount = null;
+        var contentLabel = null;
+        var contentSubLabel = null;
+        var accountDescription = null;
+        var description = null;
+        
         switch (type) {
             case "bet":
+                amount = data.unitStake;
                 if (amount < 5.01) {
                     gradient = "bgpink";
                 } else if (amount < 15.01) {
@@ -37,15 +157,31 @@ define(["jquery", "mustache", "text!../templates/box_template.html", "abpify"], 
                 } else {
                     gradient = "bggold";
                 }
+                contentLabel = "£" + amount;
+                contentSubLabel = data.transactionSubTypeDescription;
+                accountDescription = data.accountDescription;
+                description = data.description;
                 break;
             case "new_account":
                 gradient = "bggreen";
+                contentLabel = "New";
+                contentSubLabel = "Account";
+                accountDescription = data.lastName + ", " + data.firstName;
+                description = data.email;
                 break;
             case "failed_bet":
                 gradient = "bgred";
+                amount = data.unitStake;
+                contentLabel = "Failed";
+                contentSubLabel = "Bet";
                 break;
             case "payrec" :
                 gradient = "bgbronze";
+                amount = data.credit > 0 ? data.credit : data.debit;
+                contentLabel = "£" + amount;
+                contentSubLabel = "Payrec";
+                accountDescription = data.account.lastName + ", " + data.account.firstName;
+                description = data.description;
                 break;
         }
 
@@ -53,12 +189,12 @@ define(["jquery", "mustache", "text!../templates/box_template.html", "abpify"], 
         // Use Mustache to populate the template
         var result = mustache.render(template, { 
             "background_gradient" : gradient,
-            "left_content" : type !== "new_account" ? "£" + amount : "New", 
-            "left_sub_label" : type === "bet" ? data.transactionSubTypeId : type === "new_account" ? "Account" : test_data.type_description, 
-            "account_description" : data.accountDescription, 
-            "account_username" : data.account.userName,
+            "left_content" : contentLabel, 
+            "left_sub_label" : contentSubLabel,
+            "account_description" : accountDescription, 
+            "account_username" : data.userName ? data.userName.toLowerCase() : data.account.userName.toLowerCase(),
             "account_number" : data.accountNumber,
-            "description" : data.description,
+            "description" : description,
             "right_footer_left" : type === "bet" ? "Potential Win £" + data.potentialWin : "",
             "right_footer_middle" : type === "bet" ? "Total Stake £" + data.betTotalCost : "",
             "right_footer_right" : ""          
